2cd1cfbb9d2cddf3ee1a3cde35f2a3261e26f722
Joe Witzke <joewitzke@joes-mbp.lan>
2021-06-11 10:07:16 -0600

    posting a new project to the db successfully

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,15 @@
-// build your `Project` model here
+const db = require('../../data/dbConfig');
+
+function getProjects() {
+  return db('projects');
+}
+
+async function insertProject(project) {
+  const [project_id] = await db('projects').insert(project);
+  return getProjects().where({ project_id }).first();
+}
+
+module.exports = {
+  getProjects,
+  insertProject,
+};
--- api/project/router.js
+++ api/project/router.js
@@ -4,11 +4,19 @@ const Project = require('./model');
 const router = express.Router();
 
 router.post('/', (req, res, next) => {
-  res.json('posting new project');
+  Project.insertProject(req.body)
+    .then((project) => {
+      res.status(201).json(project);
+    })
+    .catch(next);
 });
 
 router.get('/', (req, res, next) => {
-  res.json('getting project');
+  Project.getProjects()
+    .then((projects) => {
+      res.status(200).json(projects);
+    })
+    .catch(next);
 });
 
 module.exports = router;
