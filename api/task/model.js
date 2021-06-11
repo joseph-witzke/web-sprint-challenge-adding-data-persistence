fb4e93fea4127d6f6f2d4a56f614f534d1b235ba
Joe Witzke <joewitzke@joes-mbp.lan>
2021-06-11 11:13:05 -0600

    task router and model written and tested in Postman

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,24 @@
-// build your `Task` model here
+const db = require('../../data/dbConfig');
+
+function getTasks() {
+  return db('tasks as t')
+    .leftJoin('projects as p', 'p.project_id', 't.project_id')
+    .select(
+      't.task_id',
+      't.task_description',
+      't.task_notes',
+      't.task_completed',
+      'p.project_name',
+      'p.project_description'
+    );
+}
+
+async function insertTask(task) {
+  const [task_id] = await db('tasks').insert(task);
+  return getTasks().where({ task_id }).first();
+}
+
+module.exports = {
+  getTasks,
+  insertTask,
+};
