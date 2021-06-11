6caa27739dccdf894e479d1262ad770b7a485fd4
Joe Witzke <joewitzke@joes-mbp.lan>
2021-06-11 09:49:24 -0600

    projects router wired with server and postman tests returning test response

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,14 @@
-// build your `/api/projects` router here
+const express = require('express');
+const Project = require('./model');
+
+const router = express.Router();
+
+router.post('/', (req, res, next) => {
+  res.json('posting new project');
+});
+
+router.get('/', (req, res, next) => {
+  res.json('getting project');
+});
+
+module.exports = router;
