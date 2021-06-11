e7f1b5e260da97c638b15ced50a549fed3923977
Joe Witzke <joewitzke@joes-mbp.lan>
2021-06-11 15:49:23 -0600

    Did some research and tried to transform my response to show true/false instead of 1/0 but couldn't get it

--- api/project/model.js
+++ api/project/model.js
@@ -3,6 +3,13 @@ const db = require('../../data/dbConfig');
 function getProjects() {
   return db('projects');
 }
+// const { project_completed } = req.body
+// const rows = await db('projects')
+// const transformed = rows.map(row => {
+//   if (!project_completed) {
+//     return { ...row, project_completed: false}
+//   }
+//   return transformed;
 
 async function insertProject(project) {
   const [project_id] = await db('projects').insert(project);
