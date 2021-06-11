812a2623ec20ea5586ba2ff27a8ac65d48b5aa73
Joe Witzke <joewitzke@joes-mbp.lan>
2021-06-11 10:41:00 -0600

    resources router and model complete. Tested post and get in postman and is successfull

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,22 @@
-// build your `/api/resources` router here
+const express = require('express');
+const Resource = require('./model');
+
+const router = express.Router();
+
+router.post('/', (req, res, next) => {
+  Resource.insertResource(req.body)
+    .then((resource) => {
+      res.status(201).json(resource);
+    })
+    .catch(next);
+});
+
+router.get('/', (req, res, next) => {
+  Resource.getResources()
+    .then((resources) => {
+      res.status(200).json(resources);
+    })
+    .catch(next);
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const projectRouter = require('./project/router');
+const resourceRouter = require('./resource/router');
 
 const server = express();
 
 server.use(express.json());
 server.use('/api/projects', projectRouter);
+server.use('/api/resources', resourceRouter);
 
 server.use((err, req, res, next) => {
   // eslint-disable-line
